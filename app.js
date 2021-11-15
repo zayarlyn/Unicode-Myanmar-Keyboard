71d2098d99954df39704e2d6f08447980825e3ad
Zeiyar Lynn <zeiyarlynn@gmail.com>
2021-11-15 16:52:09 +0630

    solved binding

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const keyboard = {
    key_layout : {
-      eng_layout : [ ["1"], ["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"], ["9"], ["0"], [""], ["q", 'ဈ', 'ဆ'], ["w", 'ဝ', 'တ'], ["e", 'ဣ', 'န'], ["r", '၎င်း', 'မ'], ["t", 'ဤ','အ'], ["y", '၌', 'ပ'], ["u", 'ဥ', 'က'], ["i", '၍', 'င'], ["o", 'ဿ','သ'], ["p", 'ဏ','စ'] ,["backspace"],["capslock"], ["a", 'ဗ', 'ေ'], ["s",'ှ','ျ'], ["d",'ီ', 'ိ'], ["f","", '်'], ["g", 'ါ', 'ွ'], ["h", 'ံ', '့'], ["j", 'ဲ', 'ြ'], ["k", 'ဒ', "ု"], ["l", 'ဓ', 'ူ'], ["enter"], ["shift"], ["z", 'ဇ', 'ဖ'], ["x", 'ဌ', 'ထ'], ["c", 'ဃ', 'ခ'], ["v", 'ဠ', 'လ'], ["b", 'ယ', 'ဘ'], ["n", 'ဉ', 'ည'], ["m", 'ဦ',  'ာ'], [",", '၊', '။'], [".", "ရ", "္"], ["?"], [""], ["[",'ဧ','ဟ'], [";", 'ဂ', 'း'], ["space"], ["\\","ႅ", '၏'], ["]",'ဪ',"ဩ"]]
+      eng_layout : [ ["1"], ["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"], ["9"], ["0"], [""], ["q", 'ဈ', 'ဆ'], ["w", 'ဝ', 'တ'], ["e", 'ဣ', 'န'], ["r", '၎င်း', 'မ'], ["t", 'ဤ','အ'], ["y", '၌', 'ပ'], ["u", 'ဥ', 'က'], ["i", '၍', 'င'], ["o", 'ဿ','သ'], ["p", 'ဏ','စ'] ,["backspace"],["shift"], ["a", 'ဗ', 'ေ'], ["s",'ှ','ျ'], ["d",'ီ', 'ိ'], ["f","", '်'], ["g", 'ါ', 'ွ'], ["h", 'ံ', '့'], ["j", 'ဲ', 'ြ'], ["k", 'ဒ', "ု"], ["l", 'ဓ', 'ူ'], ["enter"], [""], ["z", 'ဇ', 'ဖ'], ["x", 'ဌ', 'ထ'], ["c", 'ဃ', 'ခ'], ["v", 'ဠ', 'လ'], ["b", 'ယ', 'ဘ'], ["n", 'ဉ', 'ည'], ["m", 'ဦ',  'ာ'], [",", '၊', '။'], [".", "ရ", "္"], ["?"], [""], ["[",'ဧ','ဟ'], [";", 'ဂ', 'း'], ["space"], ["\\","ႅ", '၏'], ["]",'ဪ',"ဩ"]]
    },
 
    keyboard_binding: false,
-   cap_lock : null,
-   shift_key : false,
+   shift_key : null,
    value : ""
 }
 
@@ -30,20 +29,14 @@ function init() {
                button.innerHTML = createCustomKey('backspace');
                button.addEventListener('click', () => {
                   inputBox.value = inputBox.value.slice(0, -1);
-                  // keyboard.cap_lock = null;
-                  // document.getElementById('k-capslock').classList.toggle('cap-on')
-               });
-               break;
-            case "capslock":
-               button.innerHTML = createCustomKey('arrow-alt-circle-up');
-               button.addEventListener('click', () => {
-                  keyboard.cap_lock = keyboard.cap_lock? null : true;
-                  button.classList.toggle('k-cap-on');
+                  keyboard.shift_key = null;
                });
                break;
             case "shift":
+               button.innerHTML = createCustomKey('arrow-alt-circle-up');
                button.addEventListener('click', () => {
-                  keyboard.shift_key = true;
+                  keyboard.shift_key = keyboard.shift_key? null : true;
+                  button.classList.toggle('cap-on');
                });
                break;
             case "enter":
@@ -51,8 +44,7 @@ function init() {
                button.firstElementChild.style.transform = "rotate(90deg)"
                button.addEventListener('click', () => {
                   inputBox.value += '\n';
-                  // keyboard.cap_lock = null;
-                  // document.getElementById('k-capslock').classList.toggle('cap-on')
+                  keyboard.shift_key = null;
                })
                break;
             case "space":
@@ -60,16 +52,16 @@ function init() {
                button.classList.add('key-common');
                button.addEventListener('click', () => {
                   inputBox.value += ' ';
-                  // keyboard.cap_lock = null;
-                  // document.getElementById('k-capslock').classList.toggle('cap-on')
+                  keyboard.shift_key = null;
                });
                break;
             default:
                button.innerText = key[0];
                button.addEventListener('click', () => {
-                  keyboard.value += keyboard.cap_lock ?  key[0].toUpperCase() : key[0];
-                  // keyboard.cap_lock = null;
-                  // document.getElementById('k-capslock').classList.toggle('cap-on')
+                  keyboard.value += keyboard.shift_key ?  key[0].toUpperCase() : key[0];
+                  keyboard.shift_key = null;
+                  document.getElementById('k-shift').classList.remove('cap-on');
+                  inputBox.value += key[0];
                });
          }
       }    
@@ -81,9 +73,8 @@ function init() {
          
          button.addEventListener('click', () => {
             inputBox.value += keyboard.shift_key? key[1] : key[2];
-            keyboard.shift_key = false;
-            // keyboard.cap_lock = null;
-            // document.getElementById('k-capslock').classList.toggle('cap-on')
+            keyboard.shift_key = null;
+            document.getElementById('k-shift').classList.remove('cap-on')
          });
       }  
       button.setAttribute('id', `k-${key[0]}`);
@@ -100,6 +91,7 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 document.addEventListener('keydown', e => {
+   console.log(e.key);
    const key = document.getElementById(`k-${e.key.toLowerCase()}`);
    if (key !== null)
       key.click();
